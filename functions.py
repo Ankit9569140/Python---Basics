2239095eaaa905d129ad2effdfd81b21aa23b2a5
Ankit Vishwakarma <iamankit9569@gmail.com>
2026-06-26 00:11:11 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,10 @@
+# greet() naam ka function banaya gaya hai
+
+def greet():
+    # Ye line function call hone par print hogi
+    print("Hello Ankit")
+
+
+# Function Call
+# Function ko execute karne ke liye call karte hain
+greet()
